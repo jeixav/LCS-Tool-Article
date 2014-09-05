4dafea97015fb3a3baff853d625c2318d1dea044
Kristjan Onu <jeixav@gmail.com>
2014-09-04 22:07:57 -0400

    Replace explicit reference numbers by LaTeX label and reference tags

--- listings/double_gyre/lambda_strain_stretch_lcs.m
+++ listings/double_gyre/lambda_strain_stretch_lcs.m
@@ -1,12 +1,12 @@
 % Strainlines LCSs
 strainlineMaxLength = 20;
 gridSpace = diff(domain(1,:))/(double(resolution(1))-1);
-strainlineLocalMaxDistance = 2*gridSpace;
+strainlineLocalMaxDistance = 2*gridSpace; %@\label{ll:strainlineLocalMaxDistance}
 
 strainlineLcs = seed_curves_from_lambda_max(strainlineLocalMaxDistance,strainlineMaxLength,cgEigenvalue(:,2),cgEigenvector(:,1:2),domain,resolution);
 
 % Stretchlines LCSs
 stretchlineMaxLength = 20;
-stretchlineLocalMaxDistance = 10*gridSpace;
+stretchlineLocalMaxDistance = 10*gridSpace;  %@\label{ll:stretchlineLocalMaxDistance}
 
 stretchlineLcs = seed_curves_from_lambda_max(stretchlineLocalMaxDistance,stretchlineMaxLength,-cgEigenvalue(:,1),cgEigenvector(:,3:4),domain,resolution);
