a6c41ec317fadf4ce211abcd1c397f4baec83a44
Kristjan Onu <jeixav@gmail.com>
2014-11-11 20:47:26 -0500

    Add code to generate convergence figure

---
+++ graphics/bickley_jet/startup.m
@@ -0,0 +1,2 @@
+addpath(fullfile('..','..','LCS-Tool','demo','bickley_jet'))
+addpath(fullfile('..','..','LCS-Tool'))
---
+++ graphics/double_gyre/startup.m
@@ -0,0 +1,3 @@
+addpath(fullfile('..','..','LCS-Tool','demo','double_gyre'))
+addpath(fullfile('..','..','LCS-Tool'))
+addpath(fullfile('..','matlab2tikz','src'))
