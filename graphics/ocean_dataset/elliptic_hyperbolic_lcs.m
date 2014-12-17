c6f05ceedcf005e74371ed86772c0f1eac19d458
Kristjan Onu <jeixav@gmail.com>
2014-12-17 09:19:15 -0500

    Update ocean demo parameters

--- graphics/ocean_dataset/elliptic_hyperbolic_lcs.m
+++ graphics/ocean_dataset/elliptic_hyperbolic_lcs.m
@@ -10,14 +10,14 @@ resolution = [resolutionX,resolutionY];
 %% Velocity definition
 load('ocean_geostrophic_velocity.mat')
 % Set velocity to zero at boundaries
-vlon(:,[1,end],:) = 0;
-vlon(:,:,[1,end]) = 0;
-vlat(:,[1,end],:) = 0;
-vlat(:,:,[1,end]) = 0;
+vLon(:,[1,end],:) = 0;
+vLon(:,:,[1,end]) = 0;
+vLat(:,[1,end],:) = 0;
+vLat(:,:,[1,end]) = 0;
 interpMethod = 'spline';
-vlon_interpolant = griddedInterpolant({time,lat,lon},vlon,interpMethod);
-vlat_interpolant = griddedInterpolant({time,lat,lon},vlat,interpMethod);
-lDerivative = @(t,x,~)derivative(t,x,vlon_interpolant,vlat_interpolant);
+vLonInterpolant = griddedInterpolant({time,lat,lon},vLon,interpMethod);
+vLatInterpolant = griddedInterpolant({time,lat,lon},vLat,interpMethod);
+lDerivative = @(t,x,~)derivative(t,x,vLonInterpolant,vLatInterpolant);
 incompressible = true;
 
 %% LCS parameters
@@ -25,8 +25,9 @@ incompressible = true;
 cgEigenvalueFromMainGrid = false;
 cgAuxGridRelDelta = .01;
 
-% Lambda-lines
-poincareSection = struct('endPosition',{},'numPoints',{},'orbitMaxLength',{});
+% Lambda lines
+poincareSection = struct('endPosition',{},'numPoints',{},...
+    'orbitMaxLength',{});
 poincareSection(1).endPosition = [3.3,-32.1;3.7,-31.6];
 poincareSection(2).endPosition = [1.3,-30.9;2.0,-31.2];
 poincareSection(3).endPosition = [4.9,-29.6;5.7,-29.6];
@@ -35,24 +36,25 @@ poincareSection(5).endPosition = [3.0,-29.3;3.5,-29.3];
 [poincareSection.numPoints] = deal(100);
 nPoincareSection = numel(poincareSection);
 for i = 1:nPoincareSection
-    rOrbit = hypot(diff(poincareSection(i).endPosition(:,1)),diff(poincareSection(i).endPosition(:,2)));
+    rOrbit = hypot(diff(poincareSection(i).endPosition(:,1)),...
+        diff(poincareSection(i).endPosition(:,2)));
     poincareSection(i).orbitMaxLength = 4*(2*pi*rOrbit);
 end
 lambda = .9:.02:1.1;
-lambdaLineOdeSolverOptions = odeset('relTol',1e-6,'initialStep',1e-2);
+lambdaLineOdeSolverOptions = odeset('relTol',1e-5,'initialStep',1e-2);
 forceEtaComplexNaN = true;
 
-% Strainlines
-strainlineMaxLength = 20;
-strainlineLocalMaxDistance = 2*deltaX;
-strainlineOdeSolverOptions = odeset('relTol',1e-6);
+% Shrink lines
+shrinkLineMaxLength = 20;
+shrinkLineLocalMaxDistance = 4*deltaX;
+shrinkLineOdeSolverOptions = odeset('relTol',1e-4);
 
-% Stretchlines
-stretchlineMaxLength = 20;
-stretchlineLocalMaxDistance = 4*deltaX;
-stretchlineOdeSolverOptions = odeset('relTol',1e-6);
+% Stretch lines
+stretchLineMaxLength = 20;
+stretchLineLocalMaxDistance = 8*deltaX;
+stretchLineOdeSolverOptions = odeset('relTol',1e-3);
 
-% Graphics properties
+% Graphic properties
 repellingColor = 'r';
 attractingColor = 'b';
 ellipticColor = [0,.6,0];
@@ -62,7 +64,11 @@ xlabel(hAxes,'Longitude (°)')
 ylabel(hAxes,'Latitude (°)')
 
 %% Cauchy-Green strain eigenvalues and eigenvectors
-[cgEigenvector,cgEigenvalue] = eig_cgStrain(lDerivative,domain,resolution,timespan,'incompressible',incompressible,'eigenvalueFromMainGrid',cgEigenvalueFromMainGrid,'auxGridRelDelta',cgAuxGridRelDelta);
+[cgEigenvector,cgEigenvalue] = eig_cgStrain(lDerivative,domain,...
+    resolution,timespan,...
+    'incompressible',incompressible,...
+    'eigenvalueFromMainGrid',cgEigenvalueFromMainGrid,...
+    'auxGridRelDelta',cgAuxGridRelDelta);
 
 % Plot finite-time Lyapunov exponent
 cgEigenvalue2 = reshape(cgEigenvalue(:,2),fliplr(resolution));
@@ -72,7 +78,8 @@ colormap(hAxes,flipud(gray))
 
 %% Elliptic LCSs
 % Plot Poincare sections
-hPoincareSection = arrayfun(@(input)plot(hAxes,input.endPosition(:,1),input.endPosition(:,2)),poincareSection,'UniformOutput',false);
+hPoincareSection = arrayfun(@(input)plot(hAxes,input.endPosition(:,1),...
+    input.endPosition(:,2)),poincareSection,'UniformOutput',false);
 hPoincareSection = [hPoincareSection{:}];
 set(hPoincareSection,'color',ellipticColor)
 set(hPoincareSection,'LineStyle','--')
@@ -80,7 +87,10 @@ set(hPoincareSection,'marker','o')
 set(hPoincareSection,'MarkerFaceColor',ellipticColor)
 set(hPoincareSection,'MarkerEdgeColor','w')
 
-[closedLambdaLinePos,closedLambdaLineNeg] = poincare_closed_orbit_range(domain,resolution,cgEigenvector,cgEigenvalue,lambda,poincareSection,'forceEtaComplexNaN',forceEtaComplexNaN,'lambdaLineOdeSolverOptions',lambdaLineOdeSolverOptions);
+[closedLambdaLinePos,closedLambdaLineNeg] = poincare_closed_orbit_range(...
+    domain,resolution,cgEigenvector,cgEigenvalue,lambda,poincareSection,...
+    'forceEtaComplexNaN',forceEtaComplexNaN,...
+    'odeSolverOptions',lambdaLineOdeSolverOptions);
 
 ellipticLcs = elliptic_lcs(closedLambdaLinePos);
 ellipticLcs = [ellipticLcs,elliptic_lcs(closedLambdaLineNeg)];
@@ -91,24 +101,29 @@ set(hEllipticLcs,'color',ellipticColor)
 set(hEllipticLcs,'linewidth',2)
 
 %% Hyperbolic repelling LCSs
-strainlineLcs = seed_curves_from_lambda_max(strainlineLocalMaxDistance,strainlineMaxLength,cgEigenvalue(:,2),cgEigenvector(:,1:2),domain,resolution,'odeSolverOptions',strainlineOdeSolverOptions);
+shrinkLine = seed_curves_from_lambda_max(...
+    shrinkLineLocalMaxDistance,shrinkLineMaxLength,cgEigenvalue(:,2),...
+    cgEigenvector(:,1:2),domain,resolution,...
+    'odeSolverOptions',shrinkLineOdeSolverOptions);
 
-% Remove strainlines inside elliptic LCSs
+% Remove shrink lines inside elliptic LCSs
 for i = 1:nPoincareSection
-    strainlineLcs = remove_strain_in_elliptic(strainlineLcs,ellipticLcs{i});
+    shrinkLine = remove_strain_in_elliptic(shrinkLine,ellipticLcs{i});
 end
 
 % Plot hyperbolic repelling LCSs
-hStrainlineLcs = cellfun(@(position)plot(hAxes,position(:,1),position(:,2)),strainlineLcs,'UniformOutput',false);
-hStrainlineLcs = [hStrainlineLcs{:}];
-set(hStrainlineLcs,'color',repellingColor)
+hRepellingLcs = cellfun(@(position)plot(hAxes,position(:,1),...
+    position(:,2)),shrinkLine,'UniformOutput',false);
+hRepellingLcs = [hRepellingLcs{:}];
+set(hRepellingLcs,'color',repellingColor)
 
 uistack(hEllipticLcs,'top')
 uistack(hPoincareSection,'top')
 
 filename = 'elliptic_repelling_lcs.tex';
 hFigure = get(hAxes,'parent');
-matlab2tikz(filename,'showInfo',false,'width','\figurewidth','figurehandle',hFigure)
+matlab2tikz(filename,'showInfo',false,'width','\figurewidth',...
+    'figurehandle',hFigure)
 
 %% Hyperbolic attracting LCSs
 hAxes = setup_figure(domain);
@@ -125,26 +140,27 @@ hEllipticLcs = copyobj(hEllipticLcs,hAxes);
 
 % FIXME Part of calculations in seed_curves_from_lambda_max are
 % unsuitable/unecessary for stretchlines do not follow ridges of λ₁
-% minimums
-stretchlineLcs = seed_curves_from_lambda_max(stretchlineLocalMaxDistance,stretchlineMaxLength,-cgEigenvalue(:,1),cgEigenvector(:,3:4),domain,resolution,'odeSolverOptions',stretchlineOdeSolverOptions);
+% minima
+stretchLine = seed_curves_from_lambda_max(...
+    stretchLineLocalMaxDistance,stretchLineMaxLength,-cgEigenvalue(:,1),...
+    cgEigenvector(:,3:4),domain,resolution,...
+    'odeSolverOptions',stretchLineOdeSolverOptions);
 
-% FIXME Discard stretchlines to prevent pdflatex from running out of memory
-idx = 1:8:numel(stretchlineLcs);
-stretchlineLcs = stretchlineLcs(idx);
-
-% Remove stretchlines inside elliptic LCSs
+% Remove stretch lines inside elliptic LCSs
 for i = 1:nPoincareSection
-    stretchlineLcs = remove_strain_in_elliptic(stretchlineLcs,ellipticLcs{i});
+    stretchLine = remove_strain_in_elliptic(stretchLine,ellipticLcs{i});
 end
 
 % Plot hyperbolic attracting LCSs
-hStretchlineLcs = cellfun(@(position)plot(hAxes,position(:,1),position(:,2)),stretchlineLcs,'UniformOutput',false);
-hStretchlineLcs = [hStretchlineLcs{:}];
-set(hStretchlineLcs,'color',attractingColor)
+hAttractingLcs = cellfun(@(position)plot(hAxes,position(:,1),...
+    position(:,2)),stretchLine,'UniformOutput',false);
+hAttractingLcs = [hAttractingLcs{:}];
+set(hAttractingLcs,'color',attractingColor)
 
 uistack(hEllipticLcs,'top')
 uistack(hPoincareSection,'top')
 
 filename = 'elliptic_attracting_lcs.tex';
 hFigure = get(hAxes,'parent');
-matlab2tikz(filename,'showInfo',false,'width','\figurewidth','figurehandle',hFigure)
+matlab2tikz(filename,'showInfo',false,'width','\figurewidth',...
+    'figurehandle',hFigure)
